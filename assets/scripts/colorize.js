126a6206fa6d3cc386befeadcd17ade2c83b420d
Sam Yong <yongmc.sam2014@outlook.com>
2018-10-15 21:29:59 -0400

    Script-colorize-add-on-fix

--- assets/scripts/colorize.js
+++ assets/scripts/colorize.js
@@ -3,9 +3,10 @@ var style = {
   "design": "yellow",
   "computer": "purple",
 }
-var text = $(".markdown-body").innerHTML
+var article = document.getElementsByClassName ('markdown-body') [0]
+var text = article.innerHTML
 Object.keys (style).forEach (function (key, i) {
-  text = text.replace (new RegExp (key, "gi"),
+  text = text.replace (new RegExp (key, "g"),
     "<span style='color: " + style [key] + "'>" + key + "</span>")
 })
-$(".markdown-body").innerHTML = text
+article.innerHTML = text
